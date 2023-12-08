5ea8ca1b7d98c23aa8ff373cbd3e6af2731d4a85
maria-geyzianny <sgeizianny@gmail.com>
2023-12-07 23:54:35 -0300

    Criação do card-verdinho

--- client/src/assets/index.ts
+++ client/src/assets/index.ts
@@ -1,21 +1,23 @@
 import colegio_integral from "./colegio_integral.png";
-import agropecuaria_tenorio_moraes from "./agropecuaria_tenorio_moraes.png"
-import grupo_ibrap from "./grupo_ibrap.png"
-import baobarba from "./baobarba.png"
-import lógica_ambiental from "./Lógica_ambiental.png"
-import habibs from "./habib's.png"
-import ball from "./ball.png"
-import refugio_do_rio_bonito from "./Refugio_do_rio_bonito.jpg"
-import AR from "./AR.png"
+import agropecuaria_tenorio_moraes from "./agropecuaria_tenorio_moraes.png";
+import grupo_ibrap from "./grupo_ibrap.png";
+import baobarba from "./baobarba.png";
+import lógica_ambiental from "./Lógica_ambiental.png";
+import habibs from "./habib's.png";
+import ball from "./ball.png";
+import refugio_do_rio_bonito from "./Refugio_do_rio_bonito.jpg";
+import AR from "./AR.png";
+import LogoCITi from "./Logo.png";
 
-
-export { colegio_integral, agropecuaria_tenorio_moraes, grupo_ibrap, baobarba, lógica_ambiental, habibs, ball, refugio_do_rio_bonito, AR  };
-
-
-
-
-
-
-
-
-
+export {
+  colegio_integral,
+  agropecuaria_tenorio_moraes,
+  grupo_ibrap,
+  baobarba,
+  lógica_ambiental,
+  habibs,
+  ball,
+  refugio_do_rio_bonito,
+  AR,
+  LogoCITi,
+};
---
+++ client/src/components/card-verdinho/style.ts
@@ -0,0 +1,16 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+  width: 26rem;
+  background-color: #005038;
+  text-align: justify;
+  padding-left: 0.5rem;
+  padding-right: 0.5rem;
+  padding-top: 0.8rem;
+  padding-bottom: 0.8rem;
+  border-radius: 1rem;
+`;
+
+export const Paragraph = styled.p`
+  font-family: "Montserrat", sans-serif;
+`;
