2d6ced7dd708fa3b825fc442210ffb16a12971ce
maria-geyzianny <sgeizianny@gmail.com>
2023-12-11 00:25:49 -0300

    Adicionado export PrimeiraSecao no index da pasta components

--- client/src/components/index.tsx
+++ client/src/components/index.tsx
@@ -2,5 +2,6 @@ import Clients from "./card-serviços";
 import CardVerdinho from "./card-verdinho";
 import Cards from "./cards";
 import CardOccupationArea from "./cardOccupationArea";
+import PrimeiraSecao from "./primeira-secao";
 
-export { Clients, Cards, CardOccupationArea, CardVerdinho };
+export { Clients, Cards, CardOccupationArea, CardVerdinho, PrimeiraSecao };
