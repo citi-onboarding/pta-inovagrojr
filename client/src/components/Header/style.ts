f38f3908354568e7dbf25bc7eb7b9344b448f680
Hugo <hugoalbuquerquedev@gmail.com>
2023-12-13 18:43:41 -0300

    feat(header): Fiz toda a estrutura do Header.

---
+++ client/src/components/Header/style.ts
@@ -0,0 +1,64 @@
+import styled from "styled-components";
+
+
+export const Head = styled.div`
+    width: 100%;
+    height: 145.547px; 
+    display: flex;
+    background-color : #FFF;
+    flex-direction: row;
+    justify-content: space-around;
+    align-items: center;
+    border: 0.5px solid #005038;
+    box-shadow: 0px 4px 4px 0px rgba(0, 0, 0, 0.25), 0px 4px 4px 0px rgba(0, 0, 0, 0.25), 0px 4px 4px 0px rgba(0, 0, 0, 0.25), 0px 4px 4px 0px rgba(0, 0, 0, 0.25);
+    list-style-type: none;
+
+
+
+    img{
+        width: 354px;
+        height: 133px;
+        margin-bottom: 25px;
+    }
+
+    a{
+        color: #252525;
+        font-family: Montserrat;
+        font-size: 30px;
+        font-style: normal;
+        font-weight: 400;
+        line-height: normal;
+        white-space: nowrap;
+
+    }
+
+    a:hover{
+        color: #0EBE68;
+        cursor: pointer;
+    }
+
+    button{
+        width: 167px;
+        height: 53.093px;
+        display: flex;
+        border-radius: 5px;
+        background: #0EBE68;
+        justify-content: center;
+        align-items: center;
+        border: none;
+    }
+    
+
+    p{
+        width: 121px;
+        height: 33.869px;
+        color: #FFF;
+        font-family: Montserrat;
+        font-size: 30px;
+        font-style: normal;
+        font-weight: 400;
+        line-height: normal;
+    }
+
+
+`
--- client/src/pages/index.tsx
+++ client/src/pages/index.tsx
@@ -1,10 +1,11 @@
 import React from "react";
-import Contato from "components/seção6";
+import Headere from "components/Header";
+
 
 export default function Home() {
   return (
     <>
-      <Contato texto="Contato:" textocaixa="Solicite seu orçamento:"/>
+      <Headere/>
     </>
   );
 }
