38e5b9eca61ed67f09bcbda93bcbcf9f00fed017
Hugo <hugoalbuquerquedev@gmail.com>
2023-12-13 14:29:26 -0300

    feat(sextasessão): adcionada a função no index do components

--- client/src/components/index.tsx
+++ client/src/components/index.tsx
@@ -6,5 +6,6 @@ import PrimeiraSecao from "./primeira-secao";
 import SegundaSecao from "./segunda-secao";
 import Section4 from "./section4";
 import Section5 from "./section5";
+import Contato from "./seção6";
 
-export { Clients, Cards, CardOccupationArea, CardVerdinho, PrimeiraSecao, SegundaSecao, Section4, Section5 };
+export { Clients, Cards, CardOccupationArea, CardVerdinho, PrimeiraSecao, SegundaSecao, Section4, Section5, Contato };
