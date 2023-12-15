32ba5c35859c6636bdbc014649e492b97d4d1374
Hugo <hugoalbuquerquedev@gmail.com>
2023-12-15 12:43:53 -0300

    feat(crudcarrossel): feito o back como solicitado.

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -1,5 +1,6 @@
 import express from "express";
 import userController from "./controllers/UserController";
+import CarrosselController from "./controllers/CarrosselController";
 
 const routes = express.Router();
 
@@ -7,5 +8,13 @@ routes.post("/user", userController.create);
 routes.get("/user", userController.get);
 routes.delete("/user/:id", userController.delete);
 routes.patch("/user/:id", userController.update);
+
+//////////////////
+
+routes.post("/carrossel", CarrosselController.create);
+routes.get("/carrossel", CarrosselController.get);
+routes.delete("/carrossel/:id", CarrosselController.delete);
+routes.patch("/carrossel/:id", CarrosselController.update);
+
 
 export default routes;
