c81527c2e1b5319f72dd6658e8ad587d40e35875
Victória <victoriaxavierq@gmail.com>
2024-01-25 18:07:08 -0300

    pegouuuu

--- server/src/server.ts
+++ server/src/server.ts
@@ -2,14 +2,21 @@ import routes from "./routes";
 import dotenv from "dotenv";
 import express from "express";
 import "@database";
+import cors from "cors";
 
 dotenv.config();
 
 const app = express();
 app.use(express.json());
+app.use(
+  cors({
+    origin: '*',
+  }),
+);
 app.use(routes);
 app.use(express.static(__dirname + "/public"));
 
 app.listen(process.env.SERVER_PORT || 3001, () => {
   console.log("📦 Server running");
 });
+
